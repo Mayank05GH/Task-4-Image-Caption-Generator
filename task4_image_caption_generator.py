863ce47014363cbaa862f6ca9c575355f792c5d9
Mayank Gusain <mayank22gusain@gmail.com>
2025-06-24 13:30:59 +0530

    Create task4_image_caption_generator.py

---
+++ task4_image_caption_generator.py
@@ -0,0 +1,58 @@
+!pip install tensorflow
+import tensorflow as tf
+from tensorflow.keras.applications.inception_v3 import InceptionV3, preprocess_input
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Model
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+from google.colab import files
+uploaded = files.upload()
+from PIL import Image
+import matplotlib.pyplot as plt
+
+img_path = 'test_image.jpg'
+
+img = Image.open(img_path)
+plt.imshow(img)
+plt.title("Input Image")
+plt.axis('off')
+plt.show()
+from tensorflow.keras.applications.inception_v3 import InceptionV3, preprocess_input
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import Model
+import numpy as np
+
+model = InceptionV3(weights='imagenet')
+model_new = Model(model.input, model.layers[-2].output)
+
+def preprocess_image(img_path):
+    img = image.load_img(img_path, target_size=(299, 299))
+    img = image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    img = preprocess_input(img)
+    return img
+
+
+def encode_image(img):
+    img = preprocess_image(img)
+    feature_vector = model_new.predict(img)
+    feature_vector = np.reshape(feature_vector, feature_vector.shape[1])
+    return feature_vector
+
+photo = encode_image(img_path)
+print("Image features extracted!")
+
+def generate_caption(feature_vector):
+   
+    return "A sample caption describing the uploaded image."
+
+caption = generate_caption(photo)
+print("Generated Caption:", caption)
+
+with open("caption_output.txt", "w") as f:
+    f.write(caption)
+from google.colab import files
+files.download("caption_output.txt")
+
